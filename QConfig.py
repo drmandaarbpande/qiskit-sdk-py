5b3ac575cc96e1b94998af42dfdf86aae5efd5cb
Dr. Mandaar B. Pande <33679032+drmandaarbpande@users.noreply.github.com>
2018-03-24 13:09:10 +0530

    Create QConfig.py

---
+++ QConfig.py
@@ -0,0 +1,9 @@
+APItoken = '95c0f73e26354b8d74386e4dc25bdea61d438d09bcf9bd03e3fe193a07d80703504d78aaadc1428bef1f6ef22103b68510d6577dfab4ba499ec3e25f436545b0'
+
+config = {
+    'url': 'https://quantumexperience.ng.bluemix.net/qx/account/profile',
+    # The following should only be needed for IBM Q users.
+    'hub': 'MY_HUB',
+    'group': 'MY_GROUP',
+    'project': 'MY_PROJECT'
+}
